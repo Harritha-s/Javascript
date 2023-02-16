5919f89e83bc2685badc84e360ec4a8bcc4155ed
711119104030 <92197718+Harritha-s@users.noreply.github.com>
2023-02-16 14:10:21 +0530

    Add files via upload

---
+++ SquareRoot.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+
+let value = prompt('Enter the value ');
+let squareRoot = value * value;
+console.log(squareRoot);
+
+/*function squareRoot(value){
+    return(value * value);
+}*/
+
+//squareRoot();
+
+// const result = Math.sqrt(value);
