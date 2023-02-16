f19f979f407aa67b2da188a6a4408111604da083
711119104030 <92197718+Harritha-s@users.noreply.github.com>
2023-02-16 14:09:16 +0530

    Add files via upload

---
+++ AddNumbers.js
@@ -0,0 +1,10 @@
+const number1 = 5; 
+// const number1 = parseInt(prompt('Enter the first number '));
+
+const number2 = 3;
+// const number2 = parseInt(prompt('Enetr the second number '));
+
+const sum = number1 + number2;
+
+console.log('The sum of ' + number1 + ' and ' + number2 + ' ' + 'is ' + sum  );
+//console.log('The sum of ${numer1} and ${number2} is ${sum}');
